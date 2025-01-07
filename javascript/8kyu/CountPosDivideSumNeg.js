1e6064d7cb6e0eacec06bb3681680fd26d988e82
Rayhan <rayhansatriabudi@gmail.com>
2025-01-07 22:56:44 +0700

    CountPosDivideSumNeg

---
+++ javascript/8kyu/CountPosDivideSumNeg.js
@@ -0,0 +1,12 @@
+const countPositivesSumNegatives = (input) => {
+    if (!Array.isArray(input) || input.length === 0) {
+        return [];
+    };
+    const positiveArray = input.filter(x => x > 0);
+    const pos = positiveArray.length;
+    const neg = input.filter(x => x < 0).reduce((prev, curr) => prev + curr, 0);
+    return [pos, neg];
+};
+
+console.log(countPositivesSumNegatives([1,1,-1,-1]))
+console.log(countPositivesSumNegatives([]))
