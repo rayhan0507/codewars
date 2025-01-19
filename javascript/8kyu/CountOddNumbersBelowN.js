46b449294b42de4c86b9f4cd7e9830520de21038
Rayhan <rayhansatriabudi@gmail.com>
2025-01-19 07:51:33 +0700

    CountOddNumbersBelowN

---
+++ javascript/8kyu/CountOddNumbersBelowN.js
@@ -0,0 +1,5 @@
+const oddCount = (n) => {
+    return Math.floor(n / 2)
+}
+console.log(oddCount(7)) // 3 = 7 / 3 = 3.5 = 3
+console.log(oddCount(5)) // 2 = 5 / 2 = 2.5 = 2
---
+++ python/8kyu/CountOddNumbersbelowN.py
@@ -0,0 +1,5 @@
+def odd_count(n):
+    return n // 2
+
+print(odd_count(7)) # 3 = 7 / 2 = 3.5 = 3
+print(odd_count(5)) # 2 = 5 / 2 = 2.5 = 2
